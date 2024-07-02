d309c61319afb35edabb542b97688225d34484a3
Rahul Mistry <22cs042@charusat.edu.in>
2024-07-02 14:40:02 +0530

    ThirdPractical Largest Among Three

---
+++ ThirdPractical/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ThirdPractical
+{
+    class Program {
+        static void Main(string[] args)
+        {
+            int var1, var2, var3;
+            Console.WriteLine("Enter first number: ");
+            var1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            var2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third number: ");
+            var3 = Convert.ToInt32(Console.ReadLine());
+            if (var1 > var2 && var1 > var3)
+                Console.WriteLine("The largest number is: " + var1);
+            else if (var2 > var1 && var2 > var3)
+                Console.WriteLine("The largest number is: " + var2);
+            else
+                Console.WriteLine("The largest number is: " + var3);
+            Console.WriteLine("This Program is written by Rahul Mistry");
+        }
+    }
+}
