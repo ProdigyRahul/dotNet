cd42cb57125bc4df161e523a6e0a2662c36fc0b7
Rahul Mistry <22cs042@charusat.edu.in>
2024-07-02 14:36:35 +0530

    Practical 2

--- prac-2/pract-2/Program.cs
+++ prac-2/pract-2/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿namespace pract2{
+    internal class Program{
+        static void Main(string[] args){
+            int var1, var2;
+            Console.WriteLine("Enter Number1: ");
+            var1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number2: ");
+            var2 = Convert.ToInt32(Console.ReadLine());
+            if((var1 > 10 && var2 <= 10) || (var2 > 10 && var1 <= 10)){
+                Console.WriteLine("Boolean test succedded both numbers are not greater than 10");
+            }
+        }
+    }
+}
